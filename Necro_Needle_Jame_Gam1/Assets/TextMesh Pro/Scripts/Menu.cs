8516d5062eb596081d1a121ca04081b6cb394b5c
warner-chris <warner.christian1991@gmail.com>
2024-05-22 03:13:17 -0400

    Added semicolon to textmesh script

--- Necro_Needle_Jame_Gam1/Assets/TextMesh Pro/Scripts/Menu.cs
+++ Necro_Needle_Jame_Gam1/Assets/TextMesh Pro/Scripts/Menu.cs
@@ -7,7 +7,7 @@ public class Menu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1)
+        SceneManager.LoadScene(1);
     }    
     
     public void QuitGame()
